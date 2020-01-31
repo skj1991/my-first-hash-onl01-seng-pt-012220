c808c4eaa374fcd1dfd945bb0468bb45ef6f88d0
Stacy Johns <stacykjohns@icloud.com>
2020-01-31 02:00:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   new_hash = {"my_name" => "Stacy", "age" => "28"}
-
 end
 
 
